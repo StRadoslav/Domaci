711b3c5f7702d04ed93120d089ddf3b0241dad24
RadoslavSt <boba.radoslav@gmail.com>
2020-07-26 17:46:47 +0200

    domaciCommit

--- cypress/integration/registerPage.spec.js
+++ cypress/integration/registerPage.spec.js
@@ -15,12 +15,10 @@ describe("Register module", () => {
     cy.get("#password").should("be.visible");
     cy.get("#password-confirmation").should("be.visible");
     cy.get(".form-check-input").should("be.visible");
-    cy.get(".form-check-label").should(
-      "have.text",
-      "Accepted terms and conditions"
-    );
+    cy.get(".form-check-label").should( "have.text", "Accepted terms and conditions");
     cy.get(".btn").should("be.visible");
   });
+
   it("GA-14 : Register page positive test - valid data ", () => {
     cy.visit("/register");
     cy.get("#first-name").type("Te");
@@ -30,11 +28,8 @@ describe("Register module", () => {
     cy.get("#password-confirmation").type("testest1");
     cy.get(".form-check-input").check();
     cy.get(".btn").click();
-    cy.get(".alert-danger").should(
-      "have.text",
-      "The email has already been taken."
-    );
-  });
+    cy.get(".alert-danger").should( "have.text", "The email has already been taken."  )
+  })
 
   it("GA-14 : Register page positive test - valid data -randomEmail", () => {
     cy.visit("/register");
@@ -45,7 +40,7 @@ describe("Register module", () => {
     cy.get("#password-confirmation").type("testest1");
     cy.get(".form-check-input").check();
     cy.get(".btn").click();
-  });
+  })
 
   it("GA-23 : Register page - visibility as logged in user ", () => {
     cy.visit("/login");
@@ -55,9 +50,9 @@ describe("Register module", () => {
     cy.get(".btn").click();
     cy.wait(5000);
     cy.get(".nav-link nav-buttons").should("be.disabled");
-  });
+  })
 
-  it.only("GA-40 : Register page test - First name input field: required", () => {
+  it("GA-40 : Register page test - First name input field: required", () => {
     cy.visit("/register");
     cy.get("#last-name").type("St");
     cy.get("#email").type(randomEmail);
@@ -65,5 +60,107 @@ describe("Register module", () => {
     cy.get("#password-confirmation").type("testest1");
     cy.get(".form-check-input").check();
     cy.get(".btn").click();
-  });
+  })
+
+it("GA-46 : Register page test - Last name input field: required", () => {
+      cy.visit("/register");
+      cy.get("#first-name").type("Te");
+      cy.get("#email").type(randomEmail);
+      cy.get("#password").type("testest1");
+      cy.get("#password-confirmation").type("testest1");
+      cy.get(".form-check-input").check();
+      cy.get(".btn").click();
+      })
+
+it("GA-54 : Register page test - Email field required", () => {
+      cy.visit("/register");
+      cy.get("#first-name").type("Te");
+      cy.get("#last-name").type("St");
+      cy.get("#password").type("testest1");
+      cy.get("#password-confirmation").type("testest1");
+      cy.get(".form-check-input").check();
+      cy.get(".btn").click();
+      })  
+
+it("GA-55 : Register page test - Email field format invalid", () => {
+      cy.visit("/register");
+      cy.get("#first-name").type("Te");
+      cy.get("#last-name").type("St");
+      cy.get("#email").type("test@test.com1@test.com")
+      cy.get("#password").type("testest1");
+      cy.get("#password-confirmation").type("testest1");
+      cy.get(".form-check-input").check();
+      cy.get(".btn").click();
+      })  
+      
+it("GA-59 : Register page test - Password input field empty", () => {
+      cy.visit("/register");
+      cy.get("#first-name").type("Te");
+      cy.get("#last-name").type("St");
+      cy.get("#email").type("test1@test.com")
+      cy.get("#password-confirmation").type("testest1");
+      cy.get(".form-check-input").check();
+      cy.get(".btn").click();
+      })    
+      
+it("GA-60 : Register page test - Password Confirm input field empty", () => {
+      cy.visit("/register");
+      cy.get("#first-name").type("Te");
+      cy.get("#last-name").type("St");
+      cy.get("#email").type("test1@test.com")
+      cy.get("#password").type("testest1");
+      cy.get(".form-check-input").check();
+      cy.get(".btn").click();
+      })   
+
+it("GA - 81 : Confirmation password doesnt match", () => {
+      cy.visit("/register");
+      cy.get("#first-name").type("Te");
+      cy.get("#last-name").type("St");
+      cy.get("#email").type("test1@test.com")
+      cy.get("#password").type("testest1");
+      cy.get("#password-confirmation").type("testest11");
+      cy.get(".form-check-input").check();
+      cy.get(".btn").click();
+      cy.get(".alert-danger").contains('The email has already been taken.').should("have.text","The email has already been taken.");
+      cy.get(".alert").contains('The password confirmation does not match.').should("have.text", "The password confirmation does not match.")
+      }) 
+      
+it("GA - 82 : Password form - invalid", () => {
+      cy.visit("/register");
+      cy.get("#first-name").type("Te");
+      cy.get("#last-name").type("St");
+      cy.get("#email").type("test1@test.com")
+      cy.get("#password").type("testetest");
+      cy.get("#password-confirmation").type("testtest");
+      cy.get(".form-check-input").check();
+      cy.get(".btn").click();
+      cy.get(".alert-danger").contains('The email has already been taken.').should("have.text", "The email has already been taken.");
+      cy.get(".alert").contains('The password confirmation does not match.').should("have.text", "The password confirmation does not match.")
+      }) 
+
+it("GA-83 : Password form - password has less then 8 characters ", () => {
+      cy.visit("/register");
+      cy.get("#first-name").type("Te");
+      cy.get("#last-name").type("St");
+      cy.get("#email").type("test1s@test.com")
+      cy.get("#password").type("test2");
+      cy.get("#password-confirmation").type("test2");
+      cy.get(".form-check-input").check();
+      cy.get(".btn").click();
+      cy.get(".alert").contains('The password must be at least 8 characters.').should("have.text", "The password must be at least 8 characters.")
+      }) 
+
+it("GA-84 : User can't register twice ", () => {
+      cy.visit("/register");
+      cy.get("#first-name").type("test");
+      cy.get("#last-name").type("test");
+      cy.get("#email").type("ruzictam@gmail.com")
+      cy.get("#password").type("testtest2");
+      cy.get("#password-confirmation").type("testtest2");
+      cy.get(".form-check-input").check();
+      cy.get(".btn").click();
+      cy.get(".alert").contains('The email has already been taken.').should("have.text" , "The email has already been taken.")
+      })       
+
 });
